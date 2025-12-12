0bc9d459bc27b4319b23794825b6548bca44ae3a
KULKERJUN <kulkerjun@gmail.com>
2025-12-12 14:26:23 +0600

    auth-ui completed and layout

---
+++ types/global.d.ts
@@ -0,0 +1,100 @@
+declare global {
+    type SignInFormData = {
+        email: string;
+        password: string;
+    };
+
+    type SignUpFormData = {
+        fullName: string;
+        email: string;
+        password: string;
+        country: string;
+        investmentGoals: string;
+        riskTolerance: string;
+        preferredIndustry: string;
+    };
+
+    type CountrySelectProps = {
+        name: string;
+        label: string;
+        control: Control;
+        error?: FieldError;
+        required?: boolean;
+    };
+
+    type FormInputProps = {
+        name: string;
+        label: string;
+        placeholder: string;
+        type?: string;
+        register: UseFormRegister;
+        error?: FieldError;
+        validation?: RegisterOptions;
+        disabled?: boolean;
+        value?: string;
+    };
+
+    type Option = {
+        value: string;
+        label: string;
+    };
+
+    type SelectFieldProps = {
+        name: string;
+        label: string;
+        placeholder: string;
+        options: readonly Option[];
+        control: Control;
+        error?: FieldError;
+        required?: boolean;
+    };
+
+    type FooterLinkProps = {
+        text: string;
+        linkText: string;
+        href: string;
+    };
+
+    type SearchCommandProps = {
+        renderAs?: 'button' | 'text';
+        label?: string;
+        initialStocks: StockWithWatchlistStatus[];
+    };
+
+    type WelcomeEmailData = {
+        email: string;
+        name: string;
+        intro: string;
+    };
+
+    type User = {
+        id: string;
+        name: string;
+        email: string;
+    };
+
+    type Stock = {
+        symbol: string;
+        name: string;
+        exchange: string;
+        type: string;
+    };
+
+    type StockWithWatchlistStatus = Stock & {
+        isInWatchlist: boolean;
+    };
+
+    type FinnhubSearchResult = {
+        symbol: string;
+        description: string;
+        displaySymbol?: string;
+        type: string;
+    };
+
+    type FinnhubSearchResponse = {
+        count: number;
+        result: FinnhubSearchResult[];
+    };
+}
+
+export {};
